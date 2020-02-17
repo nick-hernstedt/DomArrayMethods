10bcc954edea012107cb1d18b9c6cec1f040edcc
copiousamounts <nick.hernstedt@gmail.com>
2020-02-17 10:42:45 -0800

    created functions to get user info and append it to div, having trouble getting it to show up though

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -22,6 +22,36 @@ async function getRandomUser() {
     name: `${user.name.first} ${user.name.last}`,
     money: Math.floor(Math.random() * 1000000)
   };
-
+  console.log(data);
   console.log(newUser);
 }
+
+// add new obj to data arr
+function addData(obj) {
+  data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM (if nothing is passed in then use data)
+function updateDOM(providedData = data) {
+  // clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach(item => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+}
+
+// format number as money https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currenct-string
+function formatMoney(number) {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,"); // 12,345.67
+}
+
+// even listeners
+addUserBtn.addEventListener("click", getRandomUser);

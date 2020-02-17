e26df09501268b17599cf4680a74cd36a6b788f7
copiousamounts <nick.hernstedt@gmail.com>
2020-02-17 10:53:43 -0800

    added function to double users money

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -26,6 +26,15 @@ async function getRandomUser() {
   console.log(newUser);
 }
 
+// double everyones money
+function doubleMoney() {
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 };
+  });
+
+  updateDOM();
+}
+
 // add new obj to data arr
 function addData(obj) {
   data.push(obj);
